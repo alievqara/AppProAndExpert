7b35568603c4e5b4349630437d2b6e9fea8de6ce
Aliev Qara <alievqara@outlook.com>
2022-07-10 22:30:13 +0400

    taskone

---
+++ homework/ExpertDocumentProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    public class ExpertDocumentProgram : ProDocumentProgram
+    {
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document Saved in pdf format");
+
+        }
+
+
+    }
+}
---
+++ homework/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace homework
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+
+
+            //HomeWork
+            //Console.WriteLine("Test Application");
+
+            //string ba = "Basic"; //Basic
+
+            //string pr = "Pro";  //Pro
+
+            //string expert = "Expert"; //Expert
+
+            ExpertDocumentProgram document = new ExpertDocumentProgram();
+            ProDocumentProgram proDocument = new ProDocumentProgram();
+            DocumentProgram basicDocument = new DocumentProgram();
+
+
+            basicDocument.OpenDocument();
+            basicDocument.EditDocument();
+            basicDocument.SaveDocument();
+
+            //proDocument.OpenDocument();
+            //proDocument.EditDocument();
+            //proDocument.SaveDocument();
+
+
+            //document.OpenDocument();
+            //document.EditDocument();
+            //document.SaveDocument();
+            
+            
+        
+
+           
+
+           
+
+
+
+
+
+        }
+
+
+    }
+
+}
+
+
